e770365b94402d3fccde28be79c20561b0144608
mihle3124 <143448727+mihle3124@users.noreply.github.com>
2024-01-03 21:44:19 -0800

    adding Resources folder with csv and database. Also added Flask file

---
+++ Binging_Netflix_Flask.py
@@ -0,0 +1,36 @@
+# Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+import psycopg2
+
+from flask import Flask, jsonify
+
+
+
+app = Flask(__name__)
+
+engine = create_engine("sqlite:///Resources/db_browser.sqlite")
+
+
+@app.route('/')
+def hello_world():
+    print ("hello world")
+
+
+@app.route('/data')
+def return_data():
+    results = engine.execute('select * from Netflix_Test_Data').all()
+    data=[]
+    for each_result in results:
+        data.append(list(each_result))
+    return jsonify(data)
+
+
+
+if __name__ =="__main__":
+    app.run(debug = True)
