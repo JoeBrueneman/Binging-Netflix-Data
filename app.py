da5b5028c1fa70a9d5eb1a5eebae3f4131718d6b
JoeBrueneman <Joebrueneman@gmail.com>
2024-01-04 20:48:48 -0800

    New web files

---
+++ app.py
@@ -0,0 +1,38 @@
+# Import the dependencies.
+import numpy as np
+
+import os
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+import psycopg2
+
+from flask import Flask, jsonify, render_template
+
+
+
+app = Flask(__name__)
+
+
+engine = create_engine("sqlite:///Resources/netflix_data_db.sqlite")
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/data')
+def return_data():
+    results = engine.execute('select * from netflix_data').all()
+    data=[]
+    for each_result in results:
+        data.append(list(each_result))
+    return jsonify(data)
+
+
+
+if __name__ =="__main__":
+    app.run(debug = True)
