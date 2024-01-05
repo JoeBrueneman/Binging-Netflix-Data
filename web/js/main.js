b6a4cd9ecef982ca69f255937f333dbc2481d772
gialiubc <141379548+gialiubc@users.noreply.github.com>
2024-01-05 12:32:20 +0800

    merged gia to julia

---
+++ web/js/main.js
@@ -0,0 +1,77 @@
+// Select DOM items
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+const hamburger = document.querySelector('.hamburger-menu');
+const sidebar = document.querySelector('.sidebar');
+
+// Functions for tab items
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    // Add border to current tab item
+    this.classList.add('tab-border');
+    // Grab content from the DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    // Add show class to display the content
+    tabContentItem.classList.add('show');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// Listen for tab item click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+// Functions for sidebar
+function toggleSidebar() {
+    sidebar.classList.toggle('active-sidebar');
+}
+
+// Listen for hamburger menu click
+hamburger.addEventListener('click', () => {
+    console.log("Hamburger clicked");
+    sidebar.classList.toggle('active-sidebar');
+});
+
+// Document Ready
+document.addEventListener('DOMContentLoaded', () => {
+});
+
+//Top-10 Table
+const buttonTopItems = document.querySelectorAll('.btn-top')
+const buttonTopTables = document.querySelectorAll('.table-top');
+//select table content
+function selectTopTable(e) {
+    removeTopTable();
+    //grab table from DOM
+    const buttonTopTable = document.querySelector(`#${this.id}-table`);
+    //add table class
+    buttonTopTable.classList.add('show-table');
+}
+function removeTopTable() {
+    buttonTopTables.forEach(item => item.classList.remove('show-table'))
+}
+//listen for button click
+buttonTopItems.forEach(item => item.addEventListener('click', selectTopTable))
+
+//Recommendation Table
+const buttonItems = document.querySelectorAll('.btn-list')
+const buttonListTables = document.querySelectorAll('.table-list');
+//select table content
+function selectTable(e) {
+    removeTable();
+    //grab table from DOM
+    const buttonListTable = document.querySelector(`#table-${this.id}`);
+    //add table class
+    buttonListTable.classList.add('show-table');
+}
+function removeTable() {
+    buttonListTables.forEach(item => item.classList.remove('show-table'))
+}
+//listen for button click
+buttonItems.forEach(item => item.addEventListener('click', selectTable))
